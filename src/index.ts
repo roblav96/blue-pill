0d0ea501c8230e71da720d7bdb90c3b4f3f9b91f
Robert Laverty <roblav96@gmail.com>
2019-03-04 09:42:39 -0500

    bump v0.1.2

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 
 const util = require('util'); Object.assign(util.inspect.defaultOptions, { breakLength: Infinity, colors: true, compact: false, depth: 2, maxArrayLength: Infinity, showHidden: false, showProxy: false, sorted: true }); Object.assign(util.inspect.styles, { boolean: 'blue', date: 'green', null: 'red', number: 'magenta', regexp: 'green', special: 'cyan', string: 'green', symbol: 'grey', undefined: 'red' });
 import ora from 'ora'
