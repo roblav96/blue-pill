9c44d9b605f0e2ae98b4f63154e96ec45cec9e67
Robert Laverty <roblav96@gmail.com>
2019-03-06 18:14:20 -0500

    bump v0.1.7

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,36 @@
 
 const util = require('util'); Object.assign(util.inspect.defaultOptions, { breakLength: Infinity, colors: true, compact: false, depth: 2, maxArrayLength: Infinity, showHidden: false, showProxy: false, sorted: true }); Object.assign(util.inspect.styles, { boolean: 'blue', date: 'green', null: 'red', number: 'magenta', regexp: 'green', special: 'cyan', string: 'green', symbol: 'grey', undefined: 'red' });
 import ora from 'ora'
-import * as fs from 'fs'
+import * as fs from 'fs-extra'
 import * as path from 'path'
 import * as schedule from 'node-schedule'
 import * as notifier from 'node-notifier'
+import * as configstore from 'configstore'
 import * as prompts from 'prompts'
+import * as loudness from 'loudness'
 const player = require('play-sound')() as { play(sound: string): void }
+const pkg = fs.readJsonSync(path.join(__dirname, '../package.json'))
+const storage = new configstore(pkg.name)
 
 
 
-let sounds = path.join(__dirname, '../sounds')
-prompts([
-	{
-		type: 'text',
-		name: 'title',
-		message: 'Task description',
-	},
-	{
-		type: 'select',
-		name: 'duration',
-		message: 'Reminder interval',
+interface Answers extends Partial<typeof answers> { }
+const answers = {
+	title: '', subtitle: ' ',
+	interval: 0, sound: '', volume: 0,
+}
+
+async function start() {
+
+	answers.title = await prompts.prompts.text({
+		message: `Task description`,
+		initial: storage.get('title.initial'),
+	} as prompts.PromptObject) as any
+	if (answers.title) storage.set('title.initial', answers.title);
+	answers.title = answers.title || `Task was not described...`
+
+	answers.interval = Number.parseInt(await prompts.prompts.select({
+		message: `Reminder interval`,
 		initial: 1,
 		choices: [
 			{ title: '5 Minutes', value: '5' },
@@ -30,44 +40,82 @@ prompts([
 			{ title: '30 Minutes', value: '30' },
 			{ title: '60 Minutes', value: '60' },
 		],
-	},
-	{
-		type: 'select',
-		name: 'sound',
-		message: 'Alert sound',
-		initial: 5,
-		choices: [{ title: 'None', value: '' }].concat(fs.readdirSync(sounds).filter(v => v.endsWith('.wav')).map(sound => ({
-			title: sound.slice(0, -4),
-			value: path.join(sounds, sound),
-		}))),
-		onState({ value }) {
-			value && player.play(value)
-		},
-	},
-],
-	{
-		onCancel(prompt, answers) {
-			throw new Error('onCancel')
-		},
-	},
-).then(answers => {
-	answers.title = answers.title || 'Digesting the blue pill...'
-	ora({ color: 'blue', spinner: 'bouncingBall', interval: 100 }).start()
-	notify(answers.title)
-	schedule.scheduleJob(`*/${answers.duration} * * * *`, date => {
-		notify(answers.title, answers.sound)
+	} as prompts.PromptObject) as any)
+
+	let soundsdir = path.join(__dirname, '../sounds')
+	let sounds = fs.readdirSync(soundsdir).reverse()
+	answers.sound = await prompts.prompts.select({
+		message: `Alert sound`,
+		initial: 1,
+		choices: [{ title: `🔕  Silent`, value: '' }].concat(
+			sounds.filter(v => v.endsWith('.wav')).map(sound => ({
+				title: sound.slice(0, -4),
+				value: path.join(soundsdir, sound),
+			}))
+		),
+		onState: function({ value }) { value && player.play(value) } as any,
+		// onRender: async function(this: prompts.PromptObject) {
+		// 	let muted = await loudness.getMuted()
+		// 	if (muted) this.hint = 'Your system volume is muted'
+		// },
+	} as prompts.PromptObject) as any
+
+	if (answers.sound) {
+		answers.volume = Number.parseInt(await prompts.prompts.select({
+			message: 'Alert volume',
+			initial: 0,
+			choices: [
+				{ title: 'Inherit system', value: '0' },
+				{ title: '█_______', value: '10' },
+				{ title: '██______', value: '25' },
+				{ title: '████____', value: '50' },
+				{ title: '████████', value: '100' },
+			],
+		} as prompts.PromptObject) as any)
+	}
+
+	let words = answers.title.split(' ')
+	let title = []
+	let max = 32
+	while (max > 0 && words.length > 0) {
+		let word = words.shift()
+		max -= word.length
+		title.push(word)
+	}
+	answers.title = title.join(' ')
+	if (words.length > 0) answers.subtitle = words.join(' ');
+
+	ora({ color: 'cyan', spinner: 'bouncingBall', interval: 100 }).start()
+
+	notify(answers)
+	schedule.scheduleJob(`*/${answers.interval} * * * *`, function() {
+		notify(answers); alert(answers)
 	})
-}).catch(function(error) {
-	console.error(`answers Error ->`, error)
+
+} start().catch(function(error) {
+	console.error(`start Error ->`, error)
 	process.exit(0)
 })
 
-function notify(title: string, sound = '') {
+function notify(answers: Answers) {
 	notifier.notify({
-		title, message: ' ',
+		title: answers.title, message: answers.subtitle,
 		icon: path.join(__dirname, '../logo/logo.png'),
 	})
-	sound && setTimeout(() => player.play(sound), 500)
 }
+
+async function alert(answers: Answers) {
+	let muted = await loudness.getMuted()
+	if (muted || !answers.sound) return;
+	let volume = await loudness.getVolume()
+	await loudness.setVolume(answers.volume)
+	await new Promise(r => setTimeout(r, 500))
+	await util.promisify(player.play)(answers.sound)
+	await loudness.setVolume(volume)
+}
+
+
+
+declare module 'prompts' { interface PromptObject { onRender(): any } }
 
 
