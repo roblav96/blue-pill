cca301cc0affc8e9f84c3403973003435f47c6ed
Robert Laverty <roblav96@gmail.com>
2020-06-11 23:46:14 -0400

    [Darwin] M  mod.ts

--- mod.ts
+++ mod.ts
@@ -1,3 +1,14 @@
 #!/usr/bin/env -S deno run
 
-import { Command } from 'https://deno.land/x/cliffy/command.ts'
+import { Command, ActionListType } from 'https://deno.land/x/cliffy/command.ts'
+
+const { options } = await new Command()
+	// comma separated list
+	.option('-l, --list <items:number[]>', 'comma separated list of numbers.')
+	// space separated list
+	.option('-o, --other-list <items:string[]>', 'space separated list of strings.', {
+		separator: ' ',
+	})
+	.parse(Deno.args)
+
+console.log(options)
