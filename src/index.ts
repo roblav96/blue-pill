b0cf805330ea89ed1d4d650e87e15be869532b2d
Robert Laverty <roblav96@gmail.com>
2020-07-15 20:09:44 -0400

    working Player

--- src/index.ts
+++ src/index.ts
@@ -12,66 +12,47 @@ import * as pify from 'pify'
 import * as schedule from 'node-schedule'
 import * as shell from 'shelljs'
 import * as which from 'which'
-import { Player } from './player'
+import { player } from './player'
 import { prompt } from 'enquirer'
 
-const storage = new Configstore(path.parse(__filename).name)
+const storage = new Configstore('blue-pill')
 
-const players = {
-	play: '--no-show-progress',
-	mpv: '--no-terminal',
-	mplayer: '',
-	afplay: '',
-	aplay: '',
-	omxplayer: '',
-	mpg123: '',
-	mpg321: '',
-	cmdmp3: '',
+const answers = {
+	title: '',
+	subtitle: ' ',
+	interval: 0,
+	sound: '',
+	volume: 0,
 }
-let player = Object.keys(players).find((v) => !!which.sync(v, { all: false, nothrow: true }))
-console.log('player ->', player)
-
-// const player = require('play-sound')() as {
-// 	player: string
-// 	players: string[]
-// 	play(sound: string, next?: (error?: Error) => void): void
-// 	// pplay(sound: string): Promise<void>
-// }
-// if (os.platform() == 'darwin') {
-// 	player.player = 'play'
-// }
-// // const pplayer = pify(player)
-// // Object.assign(player, { pplay: util.promisify(player.play) })
-// // console.log('player ->', player)
+interface Answers extends Partial<typeof answers> {}
 
 process.nextTick(async () => {
-	console.time(`which`)
-	for (let i = 0; i < 100; i++) {
-		let player = which.sync('afplay', { nothrow: true })
-		// let player = shell.which('afplay').toString()
-		console.log('player ->', player)
-	}
-	console.timeEnd(`which`)
+	let { title = '' } = await prompt({
+		format: (value) => value.trim(),
+		initial: storage.get('prompt.initial.title'),
+		message: 'Task description',
+		name: 'title',
+		required: true,
+		result: (value) => value.trim(),
+		type: 'input',
+		validate: (value) => !!value.trim(),
+	})
+	console.log(`title ->`, title)
 
 	return
 
+	let sounds = await fs.readdir(path.join(__dirname, 'assets'))
+
 	let sound = path.join(__dirname, 'assets/Ping.ogg')
 	console.log('sound ->', sound)
-	let played = await execa.node
-
-	// console.log('player ->', player)
-	// let played = player.play(sound, function (error) {
-	// 	if (error) console.error(`player.play -> %O`, error)
-	// })
-	// console.log('played ->', played)
-
-	let answer = await prompt({
-		message: 'Task description',
-		name: 'description',
-		type: 'input',
-	})
-	console.log('answer ->', answer)
+	let played = await player.play(sound)
+	console.log('played ->', played)
 })
+
+if (!process.DEVELOPMENT) {
+	process.once('uncaughtException', () => process.exit(0))
+	process.once('unhandledRejection', () => process.exit(0))
+}
 
 // import ora from 'ora'
 // import * as fs from 'fs-extra'
--- src/player.ts
+++ src/player.ts
@@ -3,7 +3,10 @@ import * as fs from 'fs-extra'
 import * as loudness from 'loudness'
 import * as path from 'path'
 import * as which from 'which'
+import * as tf from 'type-fest'
 
+type Options = ConstructorParameters<typeof Player>[0]
+export interface Player extends Options {}
 export class Player {
 	static players = {
 		play: '--no-show-progress',
@@ -16,22 +19,24 @@ export class Player {
 		mpg321: '',
 		cmdmp3: '',
 	}
-	constructor(public options = {} as Partial<{ args: string; player: string }>) {
-		if (!options.player) {
-			options.player = Object.keys(Player.players).find(
+	constructor(options = {} as Partial<{ args: string; player: string }>) {
+		Object.assign(this, options)
+		if (!this.player) {
+			this.player = Object.keys(Player.players).find(
 				(player) => !!which.sync(player, { all: false, nothrow: true }),
 			)
-			if (!options.player) {
+			if (!this.player) {
 				throw new Error(`Could not find a player executable binary`)
 			}
 		}
-		if (!options.args) {
-			options.args = Player.players[options.player] || ''
+		if (!this.args) {
+			this.args = Player.players[this.player] || ''
 		}
 	}
 	async play(sound: string) {
-		let args = this.options.args.split(' ').filter(Boolean)
-		let played = await execa(this.options.player, args.concat(sound))
-		console.log('played ->', played)
+		let args = this.args.split(' ').filter(Boolean)
+		await execa(this.player, args.concat(sound))
 	}
 }
+
+export const player = new Player()
